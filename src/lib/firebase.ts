1229548110ce2f19d564ee0e44adf9098af8a7e0
Jatin Vishwakarma <119022686+coddingjatin@users.noreply.github.com>
2026-06-10 23:02:51 +0530

    Clear Firebase configuration values
    Removed sensitive Firebase configuration details.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA09jQgHCXfg0hNH085BSqZ3jNXEIItRys",
-  authDomain: "cold-chain-project-767f2.firebaseapp.com",
-  projectId: "cold-chain-project-767f2",
-  storageBucket: "cold-chain-project-767f2.firebasestorage.app",
-  messagingSenderId: "555465093103",
-  appId: "1:555465093103:web:d2928f3247c8fcf562d01a",
-  measurementId: "G-NQD9NRF2H1"
+  apiKey: "",
+  authDomain: "",
+  projectId: "2",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
